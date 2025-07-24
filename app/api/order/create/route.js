cbe5f047efc2223a6834489dc10e0671da8080fe
gaffar273 <agstestcr@gmail.com>
2025-07-25 00:33:02 +0530

    c

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -21,7 +21,7 @@ export async function POST (request){
         },0)
 
         await inngest.send({
-            name:'order/create',
+            name:'order/created',
             data:{
                 userId,
                 address,
