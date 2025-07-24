095bc9e50434074683425448dcd5051f6002197b
gaffar273 <agstestcr@gmail.com>
2025-07-25 01:38:05 +0530

    xjdo

--- models/order.js
+++ models/order.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+/*import mongoose from "mongoose"
 
 
 const orderSchema=new mongoose.Schema({
@@ -17,3 +17,23 @@ const orderSchema=new mongoose.Schema({
 const Order=mongoose.models.order || mongoose.model('order',orderSchema)
 
 export default Order
+*/
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: String, required: true, ref: "user" },
+  items: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  amount: { type: Number, required: true },
+  address: { type: mongoose.Schema.Types.ObjectId, ref: "address", required: true },
+  status: { type: String, required: true, default: "order Placed" },
+  date: { type: Number, required: true },
+});
+
+const Order = mongoose.models.order || mongoose.model("order", orderSchema);
+
+export default Order;
