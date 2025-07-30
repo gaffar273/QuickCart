11bddf247c590efb22c3aeddb1b15f5fb8eb6bbb
gaffar273 <agstestcr@gmail.com>
2025-07-30 16:40:14 +0530

    f

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dnelcxbdu/**',
+      },
+    ],
   }
 };
 
